4c4e0c4408c364827ee09992a5c33dd4122b00b6
Ubuntu <ubuntu@ip-172-31-93-49.ec2.internal>
2019-09-12 17:12:53 +0000

    commit added hello world CPP

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2019 Patrick Gemmell All rights reserved.
+//
+// Created by: Patrick Gemmell
+// Created on: September 12 2019
+// This is the hello world program on CPP
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from AWS Cloud9!" << std::endl;
+}
